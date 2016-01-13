a9768b333bd1db27232b54dd3fe9b4dd59640e92
avihebbar <avihebbar@gmail.com>
2016-01-13 19:22:05 +0530

    Knapsack working

--- Algorithms/Dynamic Programming/knapsack.py
+++ Algorithms/Dynamic Programming/knapsack.py
@@ -1,6 +1,6 @@
-knapsackSize = 165
-weights = [0, 23, 31, 29, 44, 53, 38, 63, 85, 89, 82]
-benefits = [0, 92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
+knapsackSize = 10#165
+weights = [0,2,3,5,7]#[0, 23, 31, 29, 44, 53, 38, 63, 85, 89, 82]
+benefits = [5,3,7,9]#[92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
 
 # solution = 1
 # 1
@@ -13,13 +13,26 @@ benefits = [0, 92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
 # 0
 # 0
 
-memo = [ [0 for i in range(len(weights)) ] for j in range(knapsackSize+1)]
-
+memo = [ [0 for i in range(knapsackSize+1) ] for j in range(len(weights))]
+used = 0*[len(weights)]
 for i in range(1, len(weights) ):
 	for j in range(1, knapsackSize+1):
 		if weights[i] > j:
-			memo[i][j] = 0
+			memo[i][j] = memo[i-1][j]
 		else:
-			memo[i][j] = min( memo[i-1][j], memo[i][j - weights[i] ] + benefits[i] )
+			memo[i][j] = max( memo[i-1][j], memo[i-1][j - weights[i] ] + benefits[i - 1] )
 
-print memo[len(weights)][knapsackSize]
+j = knapsackSize
+i = len(weights) - 1
+
+while i >= 1 and j >=	1:
+	if memo[i-1][j] == memo[i][j]:
+		print "0"
+	else:
+		print "1"
+	i = i - 1
+	j = j - 1
+
+print memo
+print memo[len(weights) - 1 ][knapsackSize]
+
